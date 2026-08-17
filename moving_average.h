520f762088c207a278c9dfe5d7ed090c2de844b5
necdetozgok-ee <necdetozgok2@gmail.com>
2026-08-17 15:44:49 +0300

    Update moving_average.h

--- moving_average.h
+++ moving_average.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-// Sensör gürültüsünü yumuşatmak için örneklem penceresi boyutu
 #define FILTER_WINDOW_SIZE 5
 
 typedef struct {
@@ -13,10 +12,8 @@ typedef struct {
     float sum;
 } MovingAverageFilter;
 
-// Filtre yapılandırmasını sıfırlayan fonksiyon
 void MovingAverage_Init(MovingAverageFilter *filter);
 
-// Yeni sensör verisi ekleyip güncel ortalamayı hesaplayan fonksiyon
 float MovingAverage_Update(MovingAverageFilter *filter, float new_value);
 
-#endif // MOVING_AVERAGE_H
+#endif 
